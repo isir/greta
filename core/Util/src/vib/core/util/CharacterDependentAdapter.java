56e694c5b3067827437c0b42738916567f092546
Philippe Gauthier <philippe.gauthier@isir.upmc.fr>
2018-08-13 15:35:10 +0200

    Now  classes implementing CharacterDepend keep a ref to their CharacterManager instance

---
+++ core/Util/src/vib/core/util/CharacterDependentAdapter.java
@@ -0,0 +1,39 @@
+/*
+ *  This file is part of VIB (Virtual Interactive Behaviour).
+ */
+package vib.core.util;
+
+/**
+ *
+ * @author Philippe Gauthier <philippe.gauthier@upmc.fr>
+ */
+public abstract class CharacterDependentAdapter implements CharacterDependent{
+    private CharacterManager characterManager;
+
+    /**
+     * @return the characterManager
+     */
+    @Override
+    public CharacterManager getCharacterManager() {
+        if(characterManager==null)
+            characterManager = CharacterManager.getStaticInstance();
+        return characterManager;
+    }
+
+    /**
+     * @param characterManager the characterManager to set
+     */
+    @Override
+    public void setCharacterManager(CharacterManager characterManager) {
+        if(this.characterManager!=null)
+            this.characterManager.remove(this);
+        this.characterManager = characterManager;
+        characterManager.add(this);
+    }
+    
+    public static CharacterManager getCharacterManagerStatic(){
+        return CharacterManager.getStaticInstance();
+    }
+    
+   
+}

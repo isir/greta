725208a7e90ba49f419c923722b0f21256273c26
Brice Donval <donval@isir.upmc.fr>
2019-11-28 16:06:08 +0100

    Corrections and homogenization into and between NetBeans projects, in order to automate the generation of manual build files in the future.

--- core/GestureAnimation/src/vib/core/animation/common/FramesReceiver.java
+++ core/GestureAnimation/src/vib/core/animation/common/FramesReceiver.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of Greta.
- * 
- * Greta is free software: you can redistribute it and / or modify
+ *
+ * Greta is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Greta is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with Greta.If not, see <http://www.gnu.org/licenses/>.
+ * along with Greta.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
 package vib.core.animation.common;
 
 import vib.core.animation.common.Frame.KeyFrame;

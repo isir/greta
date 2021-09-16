75c615b388b4520e46ee48d76e73897767119c68
Sean Graux <sean.graux@laposte.net>
2021-09-16 08:50:27 +0200

    Modify SignalForwarder to send signals one by one
    This is a test to understand how Signals are passed and performed. It will be a standalone module in the future and SignalForwarder will be restored.

--- auxiliary/SampleModule/src/greta/auxiliary/sampleModule/SignalForwarder.java
+++ auxiliary/SampleModule/src/greta/auxiliary/sampleModule/SignalForwarder.java
@@ -18,15 +18,35 @@ import java.util.List;
  * @author Philippe Gauthier <philippe.gauthier@sorbonne-universite.fr>
  */
 public class SignalForwarder implements SignalPerformer, SignalEmitter{
+    
+    private double currentStart = 0.0;
+    private int currentColorNumber = 1;
 
     protected List<SignalPerformer> performerList = new ArrayList<>();
     @Override
     public void performSignals(List<Signal> list, ID id, Mode mode) {
-        for(SignalPerformer sp : performerList){
+        
+        System.out.println("\u001b[30m*********** Start of " + id + " ********** V2");
+        currentStart = 0.0;
+        currentColorNumber = 1;
+        
+        list.forEach((currentSignal) -> {
+            if(currentSignal.getStart().getValue() > currentStart){
+                currentStart = currentSignal.getStart().getValue();
+                currentColorNumber += 1;
+            }
+            System.out.println("\u001b[3" + currentColorNumber + "m" + currentSignal + " --- " + currentSignal.getStart() + " --- " + currentSignal.getEnd());
+            List<Signal> currentSignalListed = new ArrayList<>();
+            currentSignalListed.add(currentSignal);
+            performerList.get(0).performSignals(currentSignalListed, id, mode);
+        }); 
+        
+        //performerList.get(0).performSignals(list, id, mode);
+        System.out.println("\u001b[30m*********** End of " + id + " **********\n");
+        /*for(SignalPerformer sp : performerList){
             System.out.println(String.format("SignalForwarder: sending %s to performer %s",id.toString(),sp.toString()));
             sp.performSignals(list, id, mode);
-            
-        }
+        }*/
     }
 
     @Override

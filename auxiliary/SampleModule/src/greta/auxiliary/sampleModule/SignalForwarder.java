d4d3a488204c02aa772df9308124fbd6dc3fb96a
Sean Graux <sean.graux@laposte.net>
2021-09-20 10:45:49 +0200

    Seperate parser and sender

--- auxiliary/SampleModule/src/greta/auxiliary/sampleModule/SignalForwarder.java
+++ auxiliary/SampleModule/src/greta/auxiliary/sampleModule/SignalForwarder.java
@@ -13,6 +13,9 @@ import greta.core.util.id.ID;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.concurrent.TimeUnit;
+import java.util.TreeMap;
+import java.util.Map;
 /**
  *
  * @author Philippe Gauthier <philippe.gauthier@sorbonne-universite.fr>
@@ -21,32 +24,41 @@ public class SignalForwarder implements SignalPerformer, SignalEmitter{
     
     private double currentStart = 0.0;
     private int currentColorNumber = 1;
+    
+    List<Signal> currentSignalListed = new ArrayList<>();
+    private TreeMap<Double, List<Signal>> treeList = new TreeMap<Double, List<Signal>>();
 
     protected List<SignalPerformer> performerList = new ArrayList<>();
     @Override
     public void performSignals(List<Signal> list, ID id, Mode mode) {
         
-        System.out.println("\u001b[30m*********** Start of " + id + " ********** V2");
-        currentStart = 0.0;
-        currentColorNumber = 1;
-        
+        /*              PARSER              */
+        /* Parse the list of signals into a */
+        /* TreeMap with keys = start time   */
         list.forEach((currentSignal) -> {
-            if(currentSignal.getStart().getValue() > currentStart){
-                currentStart = currentSignal.getStart().getValue();
-                currentColorNumber += 1;
+            if(currentSignal.getStart().getValue() == currentStart || currentSignal.getStart().getValue() == 0.0){
+                currentSignalListed.add(currentSignal);
             }
-            System.out.println("\u001b[3" + currentColorNumber + "m" + currentSignal + " --- " + currentSignal.getStart() + " --- " + currentSignal.getEnd());
-            List<Signal> currentSignalListed = new ArrayList<>();
-            currentSignalListed.add(currentSignal);
-            performerList.get(0).performSignals(currentSignalListed, id, mode);
-        }); 
+            else{
+                treeList.put(currentStart, currentSignalListed);
+                currentSignalListed = new ArrayList<>();
+                currentSignalListed.add(currentSignal);
+                currentStart = currentSignal.getStart().getValue();
+            }
+        });
         
-        //performerList.get(0).performSignals(list, id, mode);
+        /*              SENDER               */
+        /*Goes through the TreeMap and sends */
+        /*the signals by burst of start times*/
+        System.out.println("\n\u001b[30m*********** Start of " + id + " **********");
+        
+        for (Map.Entry<Double, List<Signal>> entry : treeList.entrySet()) {
+            System.out.println("\u001b[3" + currentColorNumber + "m  [" + currentColorNumber + "]        " + entry.getKey() + "         " + entry.getValue());
+            currentColorNumber++;
+            performerList.get(0).performSignals(entry.getValue(), id, mode);
+        }
+        
         System.out.println("\u001b[30m*********** End of " + id + " **********\n");
-        /*for(SignalPerformer sp : performerList){
-            System.out.println(String.format("SignalForwarder: sending %s to performer %s",id.toString(),sp.toString()));
-            sp.performSignals(list, id, mode);
-        }*/
     }
 
     @Override

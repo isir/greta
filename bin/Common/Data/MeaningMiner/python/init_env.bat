26cbd6623f352b5a0249c82fb27ff3261a5b7143
sagatake <63580119+sagatake@users.noreply.github.com>
2024-09-06 15:11:19 +0200

    Fix NVBG

--- bin/Common/Data/MeaningMiner/python/init_env.bat
+++ bin/Common/Data/MeaningMiner/python/init_env.bat
@@ -3,3 +3,4 @@ call conda activate py310_mm
 cd %~dp0
 call pip install -r requirements.txt
 call python -c "import nltk;nltk.download('bcp47')"
+call python -c "import nltk;nltk.download('wordnet')"
--- bin/NVBG/src/charniak/run-parser.bat
+++
@@ -1,2 +0,0 @@
-
-start /MIN charniak-windows\parseIt.exe charniak-windows/DATA/EN/ -T40 %1

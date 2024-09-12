ee231dd0547dfcef6cba24391a0daa775da26411
sagatake <63580119+sagatake@users.noreply.github.com>
2024-09-12 09:32:19 +0200

    Bug fix in charniak-parser related to NVBG

--- bin/charniak/run-parser.bat
+++ bin/charniak/run-parser.bat
@@ -1,4 +1,7 @@
+@echo off
+
 REM start /MIN charniak-windows\parseIt.exe charniak-windows/DATA/EN/ -T40 %1
+
 IF %1==FR (
     start /MIN charniak-windows\parseIt.exe charniak-windows/DATA/FR/ -T40
 ) ELSE (

4510f38769c6d80e6327683884ea05023d282cd1
Lucie Galland <lucie.galland@isir.upmc.fr>
2024-09-16 16:29:19 +0900

    fix bug 2

--- auxiliary/LLM/src/greta/auxiliary/llm/MistralFrameIncremental.java
+++ auxiliary/LLM/src/greta/auxiliary/llm/MistralFrameIncremental.java
@@ -423,10 +423,10 @@ public class MistralFrameIncremental extends LLMFrame{
                                             if(answ.contains("START:")){
                                                 System.out.println("TEXTE:"+answ);
                                                 answ = answ.replace("START:", "");
-                                                String file=TextToFML(answ, true);
+                                                String file=TextToFML(answ, false);
                                                 answ=null;
                                                 
-                                                load(file, CompositionType.replace);
+                                                load(file, CompositionType.append);
 //                                                load(file, CompositionType.append);
 
                                             }

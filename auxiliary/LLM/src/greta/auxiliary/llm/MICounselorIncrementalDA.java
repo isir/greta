ded27fc6c94ea45515a1c9da768e6b644f398608
Lucie Galland <lucie.galland@isir.upmc.fr>
2024-09-17 15:30:15 +0900

    END_CONVO

--- auxiliary/LLM/src/greta/auxiliary/llm/MICounselorIncrementalDA.java
+++ auxiliary/LLM/src/greta/auxiliary/llm/MICounselorIncrementalDA.java
@@ -467,6 +467,7 @@ public class MICounselorIncrementalDA extends LLMFrame{
                                             else{
                                                 System.out.println("TEXTE:"+answ);
                                                 String file=TextToFML(answ, false);
+                                                answ = answ.replace("END_CONVO", "");
                                                 answ=null;                                            
                                                 load(file, CompositionType.append);                                                
                                             }

c20d1700b06a298b5834267a308772362705891f
sagatake <takeshi-sa@live.jp>
2024-12-14 20:10:34 +0100

    Fixed bugs, add example config/FML

--- bin/Common/Data/microphone/check_env.py
+++ bin/Common/Data/microphone/check_env.py
@@ -3,14 +3,25 @@ import sys
 import subprocess
 import pprint as pp
 
+tgt_env_name = 'py311_mic'
+
 basedir = os.path.dirname(__file__)
 os.chdir(basedir)
 
 result = subprocess.run('check_env.bat', capture_output=True).stdout.decode('utf8').split('\r\n')
-result = ''.join(result)
+# result = ''.join(result)
 # pp.pprint(result)
 # print(result)
 
-status = 'py311_vad' in result
+for line in result:
+    line = line.split(' ')[0]
+    # print(line)
+    if tgt_env_name == line:
+        status = 1
+    else:
+        status = 0
+    if status == 1:
+        break
 
 print(int(status))
+
